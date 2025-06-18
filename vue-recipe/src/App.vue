836824d0b8806e4311587869f70e273f6bd4ba07
Amruta Patil <deshpandeamruta1991@gmail.com>
2025-06-18 16:52:07 +0530

    update done for web

--- vue-recipe/src/App.vue
+++ vue-recipe/src/App.vue
@@ -1,21 +1,49 @@
 <template>
   <v-app>
-    <!-- App Bar -->
-    <!-- <v-img
-      src="/public/Images/bg.jpg"
-      cover
-      class="position-fixed inset-0 z-[-1] opacity-30"
-    /> -->
-      <Navbar />
-    
+    <!-- Adding slogan  -->
+    <div class="bg-[#F8BBD0] text-white">
+      <div
+        class="w-full sticky top-0 z-50 bg-yellow-400 text-black py-2 shadow-md overflow-hidden"
+      >
+        <div class="marquee-bounce">
+          🍽️ Explore Delicious Recipes – Cook with Love, Eat with Joy! 💛
+        </div>
+      </div>
+      <!-- menu bar  -->
+      <div class="max-w-[1200px] mx-auto">
+        <Navbar />
+      </div>
+    </div>
 
-    <!-- Page Content -->
-    <v-main>
-      <router-view />
+    <v-main class="py-6 px-4">
+      <div class="max-w-[1200px] mx-auto">
+        <router-view />
+      </div>
     </v-main>
+    <Footer />
   </v-app>
 </template>
+<style scoped>
+.marquee-bounce {
+  display: inline-block;
+  white-space: nowrap;
+  animation: bounce-scroll 15s ease-in-out infinite alternate;
+  padding-left: 100%;
+  /* gives space to move back and forth */
+}
+
+@keyframes bounce-scroll {
+  0% {
+    transform: translateX(0%);
+  }
+
+  100% {
+    transform: translateX(-100%);
+  }
+}
+</style>
 
 <script setup>
 import Navbar from "./components/Header/Navbar.vue";
+import Footer from "./components/Footer.vue";
 </script>
--- vue-recipe/src/components/Header/Navbar.vue
+++ vue-recipe/src/components/Header/Navbar.vue
@@ -1,30 +1,81 @@
 <template>
-  <div class=" flex justify-between  items-center   ">
- 
-    <div>
-      <img class="w-80" src="/Images/logo.png" alt="logo" />
-    </div>
+  <v-container fluid class="py-2">
+    <v-row align="center" justify="space-between" class="flex-wrap">
+      <!-- Logo -->
+      <v-col cols="6" sm="3">
+        <v-img src="/Images/logo.png" alt="logo" max-width="150" contain />
+      </v-col>
 
-    <!-- Navigation Menu -->
-    <div class="flex items-end gap-6 ">
-      <router-link to="/">
-        <v-btn variant="tonal">Home</v-btn>
-      </router-link>
-      <router-link to="/Recipes">
-        <v-btn variant="tonal" >Recipes</v-btn>
-      </router-link>
+      <!-- Icon for Mobile -->
+      <v-col class="d-sm-none text-right">
+        <v-btn icon @click="drawer = !drawer">
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+      </v-col>
 
-      <router-link to="/About">
-        <v-btn variant="tonal">  About Us</v-btn>
-      </router-link>
-      <div><v-btn density="compact" icon="mdi-magnify"/></div>
-         
-      
-    
+      <!-- Desktop Menu Navbar -->
+      <v-col cols="12" sm="9" class="d-none d-sm-flex">
+        <v-row justify="end" class="gap-4" align="center" no-gutters>
+          <router-link to="/">
+            <v-btn
+              variant="tonal"
+              class="!bg-orange-500 hover:!bg-orange-600 text-none"
+              color="white"
+            >
+              Home
+            </v-btn>
+          </router-link>
 
-    </div>
-  
-  </div>
+          <router-link to="/Recipes">
+            <v-btn
+              variant="tonal"
+              class="!bg-orange-500 hover:!bg-orange-600 text-none"
+              color="white"
+            >
+              Recipes
+            </v-btn>
+          </router-link>
+
+          <router-link to="/About">
+            <v-btn
+              variant="tonal"
+              class="!bg-orange-500 hover:!bg-orange-600 text-none"
+              color="white"
+            >
+              About Us
+            </v-btn>
+          </router-link>
+
+          <SearchRecipe />
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <!--  Navbar Mobile Drawer  -->
+  <v-navigation-drawer
+    v-model="drawer"
+    temporary
+    location="right"
+    class="d-sm-none"
+  >
+    <v-list nav dense>
+      <v-list-item to="/" @click="drawer = false">
+        <v-list-item-title>Home</v-list-item-title>
+      </v-list-item>
+      <v-list-item to="/Recipes" @click="drawer = false">
+        <v-list-item-title>Recipes</v-list-item-title>
+      </v-list-item>
+      <v-list-item to="/About" @click="drawer = false">
+        <v-list-item-title>About Us</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
+<script setup>
+import { ref } from "vue";
+import SearchRecipe from "../SearchRecipe.vue";
 
+const drawer = ref(false);
+</script>
